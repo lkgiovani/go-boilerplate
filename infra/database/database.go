c32d8483e4f754db13c7ac53dab8ca773421099e
Giovani Kauã Queiroz de Souza <giovani.souza@insidecode.io>
2026-01-26 15:35:34 -0300

    feat: Implement database connection management with GORM and environment variable configuration

---
+++ infra/database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect() error {
+	dsn := getDSN()
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Database connected successfully")
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func getDSN() string {
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	password := getEnv("DB_PASSWORD", "postgres")
+	dbname := getEnv("DB_NAME", "postgres")
+	sslmode := getEnv("DB_SSLMODE", "disable")
+
+	return "host=" + host + " port=" + port + " user=" + user +
+		" password=" + password + " dbname=" + dbname + " sslmode=" + sslmode
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
